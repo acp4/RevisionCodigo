69ce332bd792d77e5b1c66c019f2eefe75598ee4
Abraham_Castillo <129539861+acp4@users.noreply.github.com>
2023-05-12 12:21:25 -0600

    Add files via upload
    revison de errores

---
+++ codigo-otros-1/assets/js/app.js
@@ -0,0 +1,32 @@
+/*
+ Renombre las variables quitandole $
+ coloque async en la funcion displayerUser
+ Se modifico la ruta del css y js para el html
+
+*/
+
+const baseEndpoint = 'https://api.github.com';
+const usersEndpoint = `${baseEndpoint}/users`;
+const n = document.querySelector('#name');
+const b = document.querySelector('#blog');
+const l = document.querySelector('.location');
+
+async function displayUser(username) { //Se agrego async
+  n.textContent = 'cargando...';
+  const response = await fetch(username);
+  const user= await response.json();
+  console.log(user);
+  n.textContent = `${user.name}`;
+  b.textContent = `${user.blog}`;
+  l.textContent = `${user.location}`;
+}
+
+function handleError(err) {
+  console.log('OH NO!');
+  console.log(err);
+  n.textContent = `Algo salió mal: ${err}`
+}
+
+displayUser(usersEndpoint)
+.then(displayUser)
+.catch(handleError);
